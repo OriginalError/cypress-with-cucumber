5ab8669997f05dfc3f9b86bb6741a273817f213c
OriginalError <OriginalError@gmail.com>
2021-05-14 21:01:46 -0400

    Moving shared files out

--- cypress/integration/common/titleAssertion.js
+++ cypress/integration/common/titleAssertion.js
@@ -3,7 +3,3 @@ import { Then, And } from "cypress-cucumber-preprocessor/steps";
 Then(`I see {string} in the title`, (title) => {
   cy.title().should('include', title)
 })
-
-And(`I can type {string} into the search bar`, (string) => {
-  cy.get('[title="Search"]').type(string)
-})
--- cypress/integration/google/google.js
+++ cypress/integration/google/google.js
@@ -4,3 +4,7 @@ const url = 'https://google.com'
 Given('I open Google page', () => {
   cy.visit(url)
 })
+
+And(`I can type {string} into the search bar`, (string) => {
+    cy.get('[title="Search"]').type(string)
+})
