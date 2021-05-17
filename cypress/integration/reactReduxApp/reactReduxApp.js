b033f5cfd380850c2244d73525c35f2dd35e4e78
OriginalError <OriginalError@gmail.com>
2021-05-17 08:16:30 -0400

    Abstraction

--- cypress/integration/reactReduxApp/reactReduxApp.js
+++ cypress/integration/reactReduxApp/reactReduxApp.js
@@ -1,36 +1,57 @@
 import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
+function getElementByTagAndId(tag, id) {
+  return cy.get(`[${tag}="${id}"]`)
+}
+
+function getCounter() {
+  return getElementByTagAndId("cy-test", "counter")
+}
+
+function getIncrementButton() {
+  return getElementByTagAndId("cy-test", "increment")
+}
+
+function getDecrementButton() {
+  return getElementByTagAndId("cy-test", "decrement")
+}
+
 // const reactUrl = 'http://localhost:3000'
 Given('I open react', () => {
   cy.visit('http://localhost:3000')
 })
 
 Then('I see the counter', () => {
-  cy.get('[cy-test="counter"]')
+  getCounter()
 })
 
 Then(`the counter is {int}`, (counterNumber) => {
-  cy.get('[cy-test="counter"]').contains(counterNumber)
+  getCounter()
+    .contains(counterNumber)
 })
 
 When(`I press the increment button`, () => {
-  cy.get('[cy-test="increment"]').click()
+  getIncrementButton()
+    .click()
 })
 
 When(`I press the increment button {int} times`, (clicks) => {
   // you can use js inline for looping - simplicity
   for (let i = 0; i < clicks; i++) { 
-    cy.get('[cy-test="increment"]').click()
+    getIncrementButton()
+      .click()
   }
 })
 
 When(`I press the decrement button`, () => {
-  cy.get('[cy-test="decrement"]').click()
+  getDecrementButton()
+    .click()
 })
 
 When(`I press the decrement button {int} times`, (clicks) => {
   // you can use js inline for looping - simplicity
   for (let i = 0; i < clicks; i++) { 
-    cy.get('[cy-test="decrement"]').click()
+    getDecrementButton()
+      .click()
   }
 })
